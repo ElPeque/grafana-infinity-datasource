06a74359a201c4d63ebdce6ebed09786449d5e45
Alejandro Martínez <elpeque2@gmail.com>
2025-02-17 22:15:17 -0300

    add script to pack the customly built plugin

---
+++ pack.sh
@@ -0,0 +1,4 @@
+
+upx gpx_infinity_linux_arm64
+tar -czvf gpx_infinity_linux_arm64.tar.gz go_plugin_build_manifest gpx_infinity_linux_arm64 plugin.json README.md module.js module.js.map CHANGELOG.md img LICENSE module.js.LICENSE.txt
+
