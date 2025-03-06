ba564fa95e536473238c76e4eb081d6f0f22d50e
Alejandro Martínez <elpeque2@gmail.com>
2025-03-05 21:02:36 -0300

    pack

--- pack.sh
+++ pack.sh
@@ -1,5 +1,9 @@
+yarn
 yarn build
 mage -v
+
 upx dist/gpx_infinity_linux_arm64
 tar -C dist -czvf gpx_infinity_linux_arm64.tar.gz go_plugin_build_manifest gpx_infinity_linux_arm64 plugin.json README.md module.js module.js.map CHANGELOG.md img LICENSE module.js.LICENSE.txt
 
+upx dist/gpx_infinity_linux_amd64
+tar -C dist -czvf gpx_infinity_linux_amd64.tar.gz go_plugin_build_manifest gpx_infinity_linux_amd64 plugin.json README.md module.js module.js.map CHANGELOG.md img LICENSE module.js.LICENSE.txt
